a599e3d4319c17c65d4a7031e054477597477262
MajorKujo <151047864+MajorKujo@users.noreply.github.com>
2024-05-13 09:40:44 +0530

    Initial commit

---
+++ cannontutorial-main/src/js/mover2.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three';
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import * as CANNON from 'cannon-es';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 20, -30);
+orbit.update();
+
+
+
+// Create cube and ground plane
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(0, 0.5, 0);
+scene.add( cube );
+const groundGeometry = new THREE.PlaneGeometry(18, 18);
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 });
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotateX(-Math.PI / 2);
+scene.add(ground);
+
+
+
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, 0, 0)
+});
+world.broadphase = new CANNON.NaiveBroadphase();
+world.solver.iterations = 10;
+
+const groundPhysMat = new CANNON.Material();
+
+const groundBody = new CANNON.Body({
+    //shape: new CANNON.Plane(),
+    //mass: 10
+    shape: new CANNON.Box(new CANNON.Vec3(18, 18, 0.1)),
+    type: CANNON.Body.STATIC,
+    material: groundPhysMat
+});
+world.addBody(groundBody);
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+const boxPhysMat = new CANNON.Material();
+
+const boxBody = new CANNON.Body({
+    mass: 1,
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    position: new CANNON.Vec3(1, 20, 0),
+    material: boxPhysMat
+});
+world.addBody(boxBody);
+
+//sphere tester
+const sphereGeo = new THREE.SphereGeometry(1);
+const sphereMat = new THREE.MeshBasicMaterial({ 
+	color: 0xff0000, 
+	wireframe: true,
+ });
+const sphereMesh = new THREE.Mesh( sphereGeo, sphereMat);
+scene.add(sphereMesh);
+
+const spherePhysMat = new CANNON.Material();
+
+const sphereBody = new CANNON.Body({
+    mass: 4,
+    shape: new CANNON.Sphere(1),
+    position: new CANNON.Vec3(0, 10, 0),
+    material: spherePhysMat
+});
+world.addBody(sphereBody);
+
+
+// Setup raycaster and mouse
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector3();
+function onMouseMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+window.addEventListener( 'mousemove', onMouseMove, false );
+
+const timeStep=1/60;
+
+function animate() {
+
+    world.step(timeStep);
+
+    ground.position.copy(groundBody.position);
+    ground.quaternion.copy(groundBody.quaternion);
+
+    cube.position.copy(boxBody.position);
+    cube.quaternion.copy(boxBody.quaternion);
+
+    sphereMesh.position.copy(sphereBody.position);
+    sphereMesh.quaternion.copy(sphereBody.quaternion);
+    
+  
+   
+	raycaster.setFromCamera( mouse, camera );
+	const intersects = raycaster.intersectObjects([ground]);
+	if(intersects.length) {
+	const { point } = intersects[0];
+	boxBody.position.copy(point.setY(0.5));
+	}
+  
+   const distance = 10;
+   raycaster.setFromCamera( mouse, camera );
+   const { origin, direction } = raycaster.ray;
+   boxBody.position.copy(origin.clone().add(direction.multiplyScalar(distance)));
+  
+  renderer.render(scene, camera);
+};
+
+renderer.setAnimationLoop(animate)
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
